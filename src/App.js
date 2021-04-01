26e65aa7d3d15828b9b5c860309806fe43b12a90
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2021-04-01 12:50:41 +0200

    added mp3 file and play and pause functionalities

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+//import styles
+import './styles/app.scss'
 //Adding components
 import Player from './components/Player';
 import Song from './components/Song';
+//import Util
+import data from './util';
 
 
 function App() {
+  //add data with State
+  const [songs, setSongs] = useState(data());
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const [currentSong, setCurrentSong] = useState(songs[1]);
   return (
     <div className="App">
-      <Song />
-      <Player/>
+      <Song currentSong={currentSong}/>
+      <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong}/>
     </div>
   );
 }
